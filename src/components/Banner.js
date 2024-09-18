435037578022dbb9eee927b4fd5f595a7d925d4b
yashampcus <yash.waikar@ampcus.com>
2024-09-17 23:58:33 -0400

    Bugfixes

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -13,7 +13,7 @@ export const Banner = () => {
   const [text, setText] = useState('');
   const [delta, setDelta] = useState(300 - Math.random() * 100);
   const [index, setIndex] = useState(1);
-  const toRotate = [ "Sofware Engineer" ];
+  const toRotate = [ "Software Engineer" ];
   const period = 2000;
 
   useEffect(() => {
