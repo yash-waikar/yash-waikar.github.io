bafce3a8e09867b18eaabf131d0c4356c004d159
yashampcus <yash.waikar@ampcus.com>
2025-01-16 17:56:59 -0500

    minor ui updates

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -71,8 +71,8 @@ export const Banner = () => {
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
             
                 <h1>{'Hey! I am Yash Waikar. A'} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Software Engineer" ]'><span className="wrap">{text}</span></span></h1>
-                  <p>Upcoming graduate from George Mason University studying Computer Science. I am passionate about Front-End Development and adapting to new Frameworks. Currently intrested in learning about cloud computing.</p>
-                  <p> I also write and make indie music on the side, listen as you scroll!</p>
+                  <p> Recent Computer Science Graduate from George Mason University studying. I am passionate about desgning building web applications and adapting to new Frameworks. Currently intrested in learning about cloud computing.</p>
+                  <p> In my free time you can find me singing and composing music, listen as you scroll!</p>
                   <iframe
                   src="https://open.spotify.com/embed/track/369Q8J2tgFH3GCFFneH8CK?utm_source=generator&theme=0"
                   style={{ borderRadius: "12px", width: "100%", height: "300px", border: "none" }}
